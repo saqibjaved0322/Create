c9af28f61ec6d6e04c4898c58f8de05a6cd1e64e
saqibjaved0322 <105146668+saqibjaved0322@users.noreply.github.com>
2024-09-21 11:32:07 +0500

    Create RXS.py

---
+++ RXS.py
@@ -0,0 +1,10 @@
+import os,platform
+os.system('git pull --quiet 2>/dev/null')
+os.system("clear")
+print('\033[92;1m [\033[97;1m•\033[92;1m] Join Whatsapp Group')
+os.system('xdg-open https://chat.whatsapp.com/KThUrljCnXiLjIAY217XHU')
+saqi=platform.architecture()[0]
+if saqi=="32bit":
+    os.system("clear");exit("\033[91;1m 32Bit Device Not Supported")
+elif saqi=="64bit":
+    __import__("BALOCH")
